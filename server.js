ca328545145fa8a61ffca36aa5d36157ba24aabb
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-02-04 23:49:43 +0000

    added all routes for notes

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-
 app.listen(PORT, () => {
     console.log(`App listening on http://localhost:${PORT}`);
   });
