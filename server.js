ba262746a0e45d7b317c994d1569493f59d86ac0
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-02-04 22:13:54 +0000

    added links for routing

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 // Sets up the linking between all the different files
 const express = require('express');
 const path = require('path');
-const apiRoutes = require('./routes/routes')
+const apiRouters = require('./routes/note')
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+// This will add the express function and tell it where to look when makeing notes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRouters);
+
+// this gets the route for the notes.hmtl
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// this gets the route for the index.hmtl
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on http://localhost:${PORT}`);
+  });
